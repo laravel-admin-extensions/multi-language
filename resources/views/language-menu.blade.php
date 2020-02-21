f83edee26541ece74a53db5fb03daf92ec26e32b
wangxk <wangxkww@foxmail.com>
2020-02-21 11:26:15 +0800

    fix #5

--- resources/views/language-menu.blade.php
+++ resources/views/language-menu.blade.php
@@ -26,7 +26,7 @@
 
     $(".language").click(function () {
         let id = $(this).data('id');
-        $.post(`/admin/locale`,{locale: id}, function () {
+        $.post("{{ admin_url('/locale') }}",{locale: id}, function () {
             location.reload();
         })
     })
